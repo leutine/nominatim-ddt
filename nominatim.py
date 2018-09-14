7150cf3037a9f58e88dcf6fa5810ae7aee873079
Eugene Leutin <leutin.e@gmail.com>
2018-09-15 01:18:00 +0300

    Added geocode function

--- nominatim.py
+++ nominatim.py
@@ -1,2 +1,18 @@
-def test():
-    pass
+import requests
+import urllib.parse
+
+
+def geocode():
+    map_object = input('Ввод ')
+    map_object_encoded = urllib.parse.quote(map_object)
+    url = 'https://nominatim.openstreetmap.org/search?q=' + \
+          map_object_encoded + \
+          '&format=json&polygon=0&addressdetails=0'
+    r = requests.get(url)
+    json_response = r.json()
+    lat = round(float(json_response[0]["lat"]), 5)
+    lon = round(float(json_response[0]["lon"]), 5)
+    print("Lat: " + str(lat), "Lon: " + str(lon))
+
+
+geocode()
