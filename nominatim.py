6506b23144a6fa9f8797e46d1e331ce27250f126
Eugene Leutin <leutin.e@gmail.com>
2018-09-16 03:24:43 +0300

    Add reverse()

--- nominatim.py
+++ nominatim.py
@@ -2,17 +2,54 @@ import requests
 import urllib.parse
 
 
-def geocode():
-    map_object = input('Ввод ')
-    map_object_encoded = urllib.parse.quote(map_object)
+def geocode(map_obj=None):
+    error_msg = "Object not found!"
+    map_object_encoded = urllib.parse.quote(map_obj)
     url = 'https://nominatim.openstreetmap.org/search?q=' + \
           map_object_encoded + \
           '&format=json&polygon=0&addressdetails=0'
-    r = requests.get(url)
-    json_response = r.json()
-    lat = round(float(json_response[0]["lat"]), 5)
-    lon = round(float(json_response[0]["lon"]), 5)
-    print("Lat: " + str(lat), "Lon: " + str(lon))
+    request = requests.get(url)
+    json_response = request.json()
+    # print(json_response)
+    if not json_response or 'error' in json_response:
+        print(error_msg)
+        return error_msg
+    else:
+        lat = round(float(json_response[0]["lat"]), 5)
+        lon = round(float(json_response[0]["lon"]), 5)
+        # print("Object: " + json_response[0]["display_name"])
+        # print("Lat: " + str(lat), "Lon: " + str(lon))
+        return lat, lon
 
 
-geocode()
+def reverse(lat: float=0.0, lon: float=0.0):
+    error_msg = "Object not found!"
+    url = 'https://nominatim.openstreetmap.org/reverse?format=json&' + \
+          'lat=' + str(lat) + \
+          '&' + \
+          'lon=' + str(lon) + \
+          '&zoom=18&addressdetails=0'
+    request = requests.get(url)
+    json_response = request.json()
+    # print(json_response)
+    if not json_response or 'error' in json_response:
+        print(error_msg)
+        return error_msg
+    else:
+        result = json_response["display_name"]
+        print(result)
+        return result
+
+
+if __name__ == "__main__":
+    menu = int(input("Geocode [0]\nReverse [1]\nEnter option: "))
+    if not menu:
+        print("<Geocode>")
+        map_object = input('Enter the name of object (city, street, site, etc.): ')
+        geocode(map_object)
+    else:
+        print("<Reverse>")
+        lat, lon = input("Enter lat and lon: ").split(",")
+        reverse(lat, lon)
+
+
--- test.py
+++ test.py
@@ -1,10 +1,23 @@
+import nominatim as nom
 import xlrd
 
 
-def test():
+def test_geocode():
     filename = "TestData/TestData.xls"
     xls = xlrd.open_workbook(filename)
-    print(xls)
+    sheet = xls.sheet_by_index(0)
+    for row_num in range(sheet.nrows):
+        row = sheet.row_values(row_num)
+        try:
+            coords = tuple(float(i) for i in str(row[1]).split(","))
+        except ValueError:
+            coords = row[1]
+        print(row[0])
+        geocode = nom.geocode(str(row[0]))
+        if geocode == coords:
+            print("Passed")
+        else:
+            print("Failed")
 
 
-test()
+test_geocode()
